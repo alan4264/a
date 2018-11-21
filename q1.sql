3fe9ee1c926511e89f03a1936ab35ec04e603d78
Alan <al.zhou@utoronto.ca>
2018-11-20 22:53:47 -0500

    a

--- q1.sql
+++ q1.sql
@@ -15,11 +15,56 @@ partyName VARCHAR(100)
 
 -- You may find it convenient to do this for each of the views
 -- that define your intermediate steps.  (But give them better names!)
-DROP VIEW IF EXISTS intermediate_step CASCADE;
+--DROP VIEW IF EXISTS intermediate_step CASCADE;
 
 -- Define views for your intermediate steps here.
+DROP VIEW IF EXISTS election_percentage CASCADE;
+CREATE VIEW election_percentage AS
+SELECT country_id, party_id, extract(year from e_date) AS year, cast(votes AS decimal)/votes_valid*100 AS e_percentage, votes, votes_valid
+FROM election_result r, election e
+WHERE r.election_id = e.id;
 
+DROP VIEW IF EXISTS yearly_percentage CASCADE;
+CREATE VIEW yearly_percentage AS
+SELECT country_id, party_id, year, avg(e_percentage) AS average_percentage
+FROM election_percentage
+WHERE year >= 1996 and year <= 2016
+GROUP BY country_id, party_id, year;
+
+DROP VIEW IF EXISTS yearly_range CASCADE;
+CREATE VIEW yearly_range AS
+(SELECT country_id, party_id, year, '(0-5]' AS voteRange
+ FROM yearly_percentage
+ WHERE average_percentage > 0 and average_percentage <= 5)
+	UNION
+(SELECT country_id, party_id, year, '(5-10]' AS voteRange
+ FROM yearly_percentage
+ WHERE average_percentage > 5 and average_percentage <= 10)
+	UNION
+(SELECT country_id, party_id, year, '(10-20]' AS voteRange
+ FROM yearly_percentage
+ WHERE average_percentage > 10 and average_percentage <= 20)
+	UNION
+(SELECT country_id, party_id, year, '(20-30]' AS voteRange
+ FROM yearly_percentage
+ WHERE average_percentage > 20 and average_percentage <= 30)
+	UNION
+(SELECT country_id, party_id, year, '(30-40]' AS voteRange
+ FROM yearly_percentage
+ WHERE average_percentage > 30 and average_percentage <= 40)
+	UNION
+(SELECT country_id, party_id, year, '(40-100]' AS voteRange
+ FROM yearly_percentage
+ WHERE average_percentage > 40 and average_percentage <= 100);
+
+DROP VIEW IF EXISTS answer CASCADE;
+CREATE VIEW answer AS 
+SELECT c.name AS countryName, p.name_short AS partyName, year, voteRange
+FROM yearly_range r, country c, party p
+WHERE r.country_id = c.id and r.party_id = p.id
+ORDER BY countryName, year, partyName;
 
 -- the answer to the query 
-insert into q1 
+insert into q1 (SELECT year, countryName, voteRange, partyName
+		FROM answer);
 
--- q2.sql
+++ q2.sql
@@ -20,9 +20,59 @@ mostRecentlyWonElectionYear INT
 DROP VIEW IF EXISTS intermediate_step CASCADE;
 
 -- Define views for your intermediate steps here.
+DROP VIEW IF EXISTS winning_vote CASCADE;
+CREATE VIEW winning_vote AS 
+SELECT election_id, max(votes) AS max_votes
+FROM election_result
+GROUP BY election_id;
 
+DROP VIEW IF EXISTS wins CASCADE;
+CREATE VIEW wins AS 
+SELECT r.election_id, party_id
+FROM election_result r, winning_vote v 
+WHERE r.election_id = v.election_id and votes = max_votes;
+
+DROP VIEW IF EXISTS wins_count CASCADE;
+CREATE VIEW wins_count AS 
+SELECT id AS party_id, count(election_id) AS wins_num, country_id
+FROM party LEFT JOIN wins ON party.id = wins.party_id
+GROUP BY id, country_id;
+
+DROP VIEW IF EXISTS country_average CASCADE;
+CREATE VIEW country_average AS 
+SELECT country_id, avg(wins_num) AS avg_wins
+FROM wins_count
+GROUP BY country_id;
+
+DROP VIEW IF EXISTS most_recent_win CASCADE;
+CREATE VIEW most_recent_win AS 
+SELECT w.party_id, w.election_id, extract(year from i.most_recent_date) AS year
+FROM	(
+	SELECT party_id, max(e_date) AS most_recent_date
+	FROM wins w, election e
+	WHERE w.election_id = e.id
+	GROUP BY party_id
+	) i, wins w, election e
+WHERE w.election_id = e.id and w.party_id = i.party_id and i.most_recent_date = e.e_date;
+
+DROP VIEW IF EXISTS more_than_three_times_average CASCADE;
+CREATE VIEW more_than_three_times_average AS 
+SELECT party_id, c.country_id, c.wins_num
+FROM wins_count c, country_average a
+WHERE c.country_id = a.country_id and wins_num > 3 * avg_wins;
+
+DROP VIEW IF EXISTS answer_without_partyfamily CASCADE;
+CREATE VIEW answer_without_partyfamily AS 
+SELECT c.name AS countryName, p.name AS partyName, m.wins_num AS wonElections, m.party_id, r.election_id AS mostRecentlyWonElectionId, r.year AS mostRecentlyWonElectionYear
+FROM more_than_three_times_average m, party p, country c, most_recent_win r
+WHERE m.party_id = p.id and p.country_id = c.id and m.party_id = r.party_id;
+
+DROP VIEW IF EXISTS answer CASCADE;
+CREATE VIEW answer AS 
+SELECT countryName, partyName, family AS partyFamily, wonElections, mostRecentlyWonElectionId, mostRecentlyWonElectionYear
+FROM answer_without_partyfamily a LEFT JOIN party_family f ON a.party_id = f.party_id;
 
 -- the answer to the query 
-insert into q2 
-
+insert into q2 (SELECT countryName, partyName, partyFamily, wonElections, mostRecentlyWonElectionId, mostRecentlyWonElectionYear
+		FROM answer);
 
